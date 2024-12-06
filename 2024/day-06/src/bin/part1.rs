bd5e129b8fa8e1e7b9cf22066a3df3c8c6d2353a
Neil Darach <neil.darach@gmail.com>
2024-12-06 10:02:23 +0000

    Day 6

---
+++ 2024/day-06/src/bin/part1.rs
@@ -0,0 +1,19 @@
+use day_06::part1::process;
+use miette::Context;
+use tracing_subscriber::fmt::format::FmtSpan;
+
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE)
+        .with_target(true)
+        .init();
+
+    let file = include_str!("../../input1.txt");
+    let result = process(file).context("process part 1")?;
+    println!("{}", result);
+    Ok(())
+}
---
+++ 2024/day-06/src/part1.rs
@@ -0,0 +1,217 @@
+use crate::custom_error::AocError;
+use std::collections::HashMap;
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let mut grid = parser::parse(input);
+    let mut visited = HashMap::new();
+    loop {
+        if grid.next_cell().is_none() {
+            break;
+        }
+        if let Some(Content::Obstacle) = grid.next_cell() {
+            grid.guard.turn_right();
+            continue;
+        }
+        visited.insert(grid.guard.cell(), ());
+        grid.guard.move_cell();
+    }
+
+    Ok((visited.keys().len() + 1).to_string())
+}
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Coord(i32, i32);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub enum Content {
+    Empty,
+    Obstacle,
+    Guard(Coord, Vector),
+}
+
+impl Content {
+    pub fn look(&self) -> Option<Coord> {
+        match self {
+            Content::Empty => None,
+            Content::Obstacle => None,
+            Content::Guard(c, v) => {
+                let delta = v.delta();
+                Some(Coord(c.0 + delta.0, c.1 + delta.1))
+            }
+        }
+    }
+    pub fn move_cell(&mut self) {
+        match self {
+            Content::Empty => {}
+            Content::Obstacle => {}
+            Content::Guard(c, v) => {
+                let delta = v.delta();
+                *c = Coord(c.0 + delta.0, c.1 + delta.1)
+            }
+        }
+    }
+    pub fn cell(&mut self) -> Coord {
+        match self {
+            Content::Empty => Coord(0, 0),
+            Content::Obstacle => Coord(0, 0),
+            Content::Guard(c, _) => *c,
+        }
+    }
+    pub fn turn_right(&mut self) {
+        match self {
+            Content::Empty => {}
+            Content::Obstacle => {}
+            Content::Guard(_, v) => {
+                *v = v.turn_right();
+            }
+        };
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub enum Vector {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Vector {
+    pub fn turn_right(self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+        }
+    }
+    pub fn delta(self) -> Coord {
+        match self {
+            Self::Up => Coord(0, -1),
+            Self::Right => Coord(1, 0),
+            Self::Down => Coord(0, 1),
+            Self::Left => Coord(-1, 0),
+        }
+    }
+}
+#[derive(Clone, Debug)]
+pub struct Grid {
+    cells: HashMap<Coord, Content>,
+    guard: Content,
+}
+
+impl Grid {
+    pub fn default() -> Self {
+        Grid {
+            cells: HashMap::default(),
+            guard: Content::Empty,
+        }
+    }
+
+    pub fn next_cell(&self) -> Option<&Content> {
+        if let Some(cell) = self.guard.look() {
+            self.cells.get(&cell)
+        } else {
+            None
+        }
+    }
+}
+
+mod parser {
+    use super::*;
+    pub fn parse(input: &str) -> Grid {
+        let mut grid = Grid::default();
+        for (y, line) in input.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                let item = match c {
+                    '.' => Content::Empty,
+                    '#' => Content::Obstacle,
+                    '^' => Content::Guard(Coord(x as i32, y as i32), Vector::Up),
+                    _ => panic!("Unrecognised symbol {} at ({},{})", c, x, y),
+                };
+                if let Content::Guard(..) = item {
+                    grid.guard = item;
+                    grid.cells.insert(Coord(x as i32, y as i32), Content::Empty);
+                } else {
+                    grid.cells.insert(Coord(x as i32, y as i32), item);
+                };
+            }
+        }
+        grid
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        assert_eq!("41", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse() -> miette::Result<()> {
+        let map = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        let grid = parser::parse(map);
+        assert_eq!(grid.cells.get(&Coord(0, 0)), Some(&Content::Empty));
+        assert_eq!(grid.cells.get(&Coord(2, 3)), Some(&Content::Obstacle));
+        Ok(())
+    }
+
+    #[test]
+    fn test_look() -> miette::Result<()> {
+        let map = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        let mut grid = parser::parse(map);
+        assert_eq!(grid.next_cell(), Some(&Content::Empty));
+        grid.guard = Content::Guard(Coord(4, 1), Vector::Up);
+        assert_eq!(grid.next_cell(), Some(&Content::Obstacle));
+        grid.guard = Content::Guard(Coord(3, 0), Vector::Right);
+        assert_eq!(grid.next_cell(), Some(&Content::Obstacle));
+        grid.guard = Content::Guard(Coord(3, 0), Vector::Left);
+        assert_eq!(grid.next_cell(), Some(&Content::Empty));
+        grid.guard.turn_right();
+        assert_eq!(grid.next_cell(), None);
+
+        grid.guard.turn_right();
+        grid.guard.turn_right();
+        grid.guard.move_cell();
+        if let Content::Guard(c, v) = grid.guard {
+            assert_eq!(Vector::Down, v);
+            assert_eq!(Coord(3, 1), c);
+        } else {
+            panic!("Not a guard");
+        }
+
+        Ok(())
+    }
+}
